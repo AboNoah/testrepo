9f5b5117804400a46c7d61e9564afd0ae935364f
AboNoah <slaehaljohi33@outlook.com>
2022-07-06 11:32:55 +0300

    added  MediaAndTextTypes_Tables

--- WebSiteTemplete/Models/IdentityModels.cs
+++ WebSiteTemplete/Models/IdentityModels.cs
@@ -9,7 +9,9 @@ namespace WebSiteTemplete.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-      public string UserType { get; set; }
+        // Here Should Add varaible in  Tables
+
+        public string UserType { get; set; }
 
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
@@ -31,6 +33,9 @@ namespace WebSiteTemplete.Models
         {
             return new ApplicationDbContext();
         }
+        // Here Should Add Models Which to Be Tables
+        public System.Data.Entity.DbSet<WebSiteTemplete.Models.MediaTypes> MediaTypes { get; set; }
 
+        public System.Data.Entity.DbSet<WebSiteTemplete.Models.TextTypes> textTypes { get; set; }
     }
 }
--- WebSiteTemplete/Models/MediaType.cs
+++ WebSiteTemplete/Models/MediaType.cs
@@ -5,9 +5,12 @@ using System.Web;
 
 namespace WebSiteTemplete.Models
 {
-    public class MediaType
+    public class MediaTypes
     {
         public int ID { get; set; }
         public string MediaTypeName { get; set; }
+        public string MediaTypeAName { get; set; }
+        public string MediaTypeEName { get; set; }
+
     }
 }
